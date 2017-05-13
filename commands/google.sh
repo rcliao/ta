bb7512a533ef6b1a76c2796fffe7997cfcae4e37
Eric Liao <rcliao01@gmail.com>
2017-05-13 13:43:00 -0700

    chore: clean up debug tests

--- commands/google.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cd webdriver-test/ && node google.js
--- webdriver-test/test/specs/hello/hello.spec.js
+++ webdriver-test/test/specs/hello/hello.spec.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
 describe('Hello world test', function() {
-	it('should get google page and its title', function() {
-		browser.url('https://google.com');
-		let title = browser.getTitle();
-		assert.equal(title, 'Google');
-	});
+    it('should get google page and its title', function() {
+        browser.url('https://google.com');
+        let title = browser.getTitle();
+        assert.equal(title, 'Google');
+    });
 });
